255c84bab14f536922a48d41fc802e35a214ef10
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-08-01 20:49:52 -0500

    Add files via upload

---
+++ Miles to Kilometers/main.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+window = Tk()
+window.title("'Sup")
+window.minsize(width=500, height=300)
+
+input_thing = Entry(width=10)
+input_thing.pack()
+
+# Label
+label = Label(text="HEY! HEEEEYYYY! I'M A LABEL, BITCH!", font=("Arial", 24))
+label.pack()
+
+def button_clicked():
+    label.config(text=input_thing.get())
+
+button = Button(text="cicke me. I dare you! WITNESS MEEEEEE!!!", command=button_clicked)
+button.pack()
+
+# input_thing = Entry(width=10)
+# input_thing.pack()
+# print(input_thing.get())
+
+window.mainloop()
