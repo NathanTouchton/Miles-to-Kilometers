19f6162a398e169b08cd760452744612cb1930a8
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-08-05 18:35:43 -0500

    Add files via upload

--- Miles to Kilometers/main.py
+++ Miles to Kilometers/main.py
@@ -8,8 +8,8 @@ input_thing = Entry(width=10)
 input_thing.pack()
 
 # Label
-label = Label(text="HEY! HEEEEYYYY! I'M A LABEL, BITCH!", font=("Arial", 24))
-label.pack()
+label = Label(text="HEY! HEEEEYYYY! I'M A LABEL", font=("Arial", 24))
+label.pack(side="left")
 
 def button_clicked():
     label.config(text=input_thing.get())
